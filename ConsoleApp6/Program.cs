f61cb8d6d4aaba023f1a7705484d9b8cb6ec5e7d
leo82380 <tkdrb062@gmail.com>
2023-04-21 00:19:51 +0900

    초기 커밋.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 입력
+            //string으로 입력받기
+            string input = Console.ReadLine();
+            //char로 입력받기
+            char charInput = char.Parse(Console.ReadLine());
+            //int로 입력받기
+            int intInput = int.Parse(Console.ReadLine());
+            //float로 입력받기
+            float floatInput = float.Parse(Console.ReadLine());
+            //double로 입력받기
+            double doubleInput = double.Parse(Console.ReadLine());
+            #endregion
+            #region 출력
+            //string 출력하기
+            Console.WriteLine("string : " + input);//+ 뒤의 input값들은 위에서 선언한 변수 이름 
+            //char 출력하기
+            Console.WriteLine("char : " + charInput);
+            //int 출력하기
+            Console.WriteLine("int : " + intInput);
+            //float
+        }
+    }
+}
